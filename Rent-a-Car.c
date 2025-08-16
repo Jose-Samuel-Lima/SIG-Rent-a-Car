0dee82b366174dcfcad40c66222dbdbf9b25a048
José-Samuel-Lima <jose.lima.14#ufrn.edu.br>
2025-08-16 16:21:05 -0300

    criação tela sobre nós

--- Rent-a-Car.c
+++ Rent-a-Car.c
@@ -4,7 +4,7 @@
 
 
 void menu_principal(void);
-// void menu_sobre(void);
+void menu_sobre(void);
 // void modulo_equipe(void);
 // void modulo_cliente(void);
 // void modulo_de_veiculos(void);
@@ -15,7 +15,7 @@ void menu_principal(void);
 
 int main(void){
     menu_principal();
-    // menu_sobre();
+    menu_sobre();
     // modulo_equipe();
     // modulo_cliente();
     // modulo_de_veiculos();
@@ -23,6 +23,7 @@ int main(void){
     // modulo_relatorios();
     return 0;
 }
+
 
 
 
@@ -67,6 +68,43 @@ void menu_principal(void){
     printf("\t\t\tTecle *Enter* para continuar..\n");
     printf("\n");
     getchar();
+}
+
+
+
+void menu_sobre(void){
+    system("clear||cls");
+    printf("\n");
+    printf("#=====================================================================#\n");
+    printf("|                                                                     |\n");
+    printf("|                        --------------------                         |\n");
+    printf("|                        | SIG - Rent a Car |                         |\n");
+    printf("|                        --------------------                         |\n");
+    printf("|                                                                     |\n");
+    printf("#=====================================================================#\n");
+    printf("|                                                                     |\n");
+    printf("|             Universidade Federal do Rio Grande do Norte             |\n");
+    printf("|                 Centro de Ensino Superior do Seridó                 |\n");
+    printf("|               Departamento de Computação e Tecnologia               |\n");
+    printf("|                 Disciplina DCT1106 -- Programação                   |\n");
+    printf("|        Projeto Sistema de Gestão para uma Locadora de Veículos      |\n");
+    printf("|          Developed by @José-Samuel-Lima -- since Aug, 2025          |\n");
+    printf("|                                                                     |\n");
+    printf("#=====================================================================#\n");
+    printf("|                                                                     |\n");
+    printf("|       O sistema de gestão da locadora de veículos Rent a Car        |\n");
+    printf("|   é uma ferramenta desenvolvida para organizar e automatizar        |\n");
+    printf("|   os processos administrativos e operacionais da empresa.           |\n");
+    printf("|   Seu principal objetivo é facilitar o controle das atividades      |\n");
+    printf("|   relacionadas à frota de veículos, aos clientes e às locações      |\n");
+    printf("|   realizadas, garantindo maior eficiência, segurança e              |\n");
+    printf("|   agilidade no atendimento.                                         |\n");
+    printf("|                                                                     |\n");
+    printf("#=====================================================================#\n");
+    printf("\n");
+    printf("\t\t\tTecle *Enter* para continuar..\n");
+    getchar();
+    printf("\n");
 }
 
 
