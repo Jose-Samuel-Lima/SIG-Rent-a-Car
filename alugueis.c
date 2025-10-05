f799824e3b1a4329661be4b04eb882eff73e6054
G-Henri <gleydson.silva.716@ufrn.edu.br>
2025-10-05 17:29:09 -0300

    Função Finalizar alugueis concluída - Main

--- alugueis.c
+++ alugueis.c
@@ -360,8 +360,35 @@ void modulo_atualizar_aluguel(void)
 
 void modulo_finalizar_aluguel(void)
 {
-    int id_aluguel;
-    int c;
+    FILE *arq_aluguel;
+    arq_aluguel = fopen("aluguel.csv","rt");
+
+    if (arq_aluguel == NULL){
+        printf("Erro ao abrir o arquivo!\n");
+        printf("Pressione Enter para continuar...");
+        getchar();
+        exit(1);
+    }
+
+    FILE *arq_aluguel_temp;
+    arq_aluguel_temp = fopen("aluguel_temp.csv","wt");
+
+    if (arq_aluguel_temp== NULL){
+        printf("Erro na criação do arquivo Temporário!\n");
+        printf("Pressione Enter para continuar...");
+        getchar();
+        exit(1);
+    }
+
+    char nome_cliente[51];
+    char cpf_cliente[12];
+    char codigo_renavam[12];
+    char modelo_veiculo[15];
+    char data_aluguel[15];
+    char id_aluguel[11];
+    char id_ler[11];
+    int c; 
+    int aluguel_encontrado = 0;
 
     system("clear||cls");
     printf("\n");
@@ -380,10 +407,39 @@ void modulo_finalizar_aluguel(void)
     printf("#=====================================================================#\n");
     printf("\n");
     printf("Informe o Id do aluguel que deseja excluir: \n");
-    scanf("%d", &id_aluguel);
+    scanf("%11s", id_ler);
     while ((c = getchar()) != '\n' && c != EOF)
         ;
-    system("cls||clear");
+    while (fscanf(arq_aluguel, "%[^;];%[^;];%[^;];%[^;];%[^;];%[^\n]\n", nome_cliente, cpf_cliente, codigo_renavam, modelo_veiculo, data_aluguel, id_aluguel) == 6) {
+
+        if (strcmp(id_aluguel, id_ler) != 0){
+
+            fprintf(arq_aluguel_temp, "%s;", nome_cliente);
+            fprintf(arq_aluguel_temp, "%s;", cpf_cliente);
+            fprintf(arq_aluguel_temp, "%s;", codigo_renavam);
+            fprintf(arq_aluguel_temp, "%s;", modelo_veiculo);
+            fprintf(arq_aluguel_temp, "%s;", data_aluguel);
+            fprintf(arq_aluguel_temp, "%s\n", id_aluguel);
+
+        }
+        else {
+            aluguel_encontrado = 1;
+        }
+    }
+
+    fclose(arq_aluguel);
+    fclose(arq_aluguel_temp);
+
+    if (aluguel_encontrado){
+            remove("aluguel.csv");
+            rename("aluguel_temp.csv","aluguel.csv");
+            printf("Aluguel finalizado com sucesso!\n");
+        }
+    else {
+        remove("aluguel_temp.csv");
+        printf("Aluguel não encontrado...\n");
+    }
+
     printf("Pressione Enter para continuar...");
     getchar();
 }
