506c0ac1821d65c15a6cddb4c5aaa78924d982ec
G-Henri <gleydson.silva.716@ufrn.edu.br>
2025-10-12 12:16:07 -0300

    Implementação de apontadores em funcionarios.h e inclusão da biblioteca "<stdbool.h>" em funcionarios.c

--- funcionarios.c
+++ funcionarios.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
 #include "funcionarios.h"
 
 int modulo_funcionario(void)
--- funcionarios.h
+++ funcionarios.h
@@ -1,20 +1,22 @@
 #ifndef FUNCIONARIO_H
 #define FUNCIONARIO_H
+#include <stdbool.h>
 
-typedef struct funcionario
-{
+typedef struct funcionario {
     char nome_funcionario[51];
     char dt_nascimento_fun[12];
     char cpf_funcionario[12];
     char cargo[22];
     char email_funcionario[30];
+    bool status;
+
 } Funcionario;
 
 int modulo_funcionario(void);
 int modulo_tela_funcionario(void);
-void modulo_cadastrar_funcionario(void);
-void modulo_dados_funcionario(void);
-void modulo_atualizar_funcionario(void);
-void modulo_excluir_funcionario(void);
+void modulo_cadastrar_funcionario(Funcionario* fun);
+void modulo_dados_funcionario(Funcionario* fun);
+void modulo_atualizar_funcionario(Funcionario* fun);
+void modulo_excluir_funcionario(Funcionario* fun);
 
 #endif
