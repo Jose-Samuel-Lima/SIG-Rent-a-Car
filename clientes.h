c8de67cee2065e0d32fb6615e14d4023cc253be8
José-Samuel-Lima <jose.lima.146@ufrn.edu.br>
2025-10-13 19:21:31 -0300

    Implementação de apontadores em clientes.h

--- clientes.h
+++ clientes.h
@@ -1,12 +1,15 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
+#include <stdbool.h>
 
 typedef struct cliente {
-    char nome_cliente[52];
+    char nome_cliente[100];
     char cpf_cliente[15];
-    char data_nascimento[15];
-    char email_cliente[52];
-    char cnh[14];
+    char data_nascimento[11];
+    char email_cliente[100];
+    char cnh[20];
+    bool status;
+    
 } Cliente;
 
 int modulo_cliente(void);
