7b9304a11e06e7cff80cdb840fc7608ce45c3e4c
G-Henri <gleydson.silva.716@ufrn.edu.br>
2025-10-26 14:22:27 -0300

    Arquivo validacao.c criado, responsável por guardar funções de validação.

--- validacao.c
+++ validacao.c
@@ -1,7 +1,55 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(){
-    printf("Hello World");
-    
-    return 0;
+// ==============================
+//       CARACTERES PADRÃO
+// ==============================
+
+int ehLetra(char c) {
+    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
+
+int ehDigito(char c) {
+    return (c >= '0' && c <= '9');
+}
+
+// ==============================
+//       DADOS ESPECÍFICOS
+// ==============================
+
+int validarNome(char *nome) {
+    for (int i = 0; nome[i] != '\0'; i++) {
+        if (!(ehLetra(nome[i]) || nome[i] == ' ')) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int validarCPF(char *cpf) {
+    int cont = 0;
+    for (int i = 0; cpf[i] != '\0'; i++) {
+        if (ehDigito(cpf[i])) cont++;
+        else if (cpf[i] != '.' && cpf[i] != '-') return 0;
+    }
+    return (cont == 11);
+}
+
+int validarEmail(char *email) {
+    return (strchr(email, '@') != NULL & strchr(email, '.') != NULL);
+}
+
+int validarData(char *data) {
+    if (strlen(data) != 10) return 0;
+    if (!(ehDigito(data[0]) && ehDigito(data[1]) && 
+          data[2] == '/' &&
+          ehDigito(data[3]) && ehDigito(data[4]) && 
+          data[5] == '/' &&
+          ehDigito(data[6]) && ehDigito(data[7]) && 
+          ehDigito(data[8]) && ehDigito(data[9]))) {
+        return 0;
+    }
+    return 1;
+
 }
