34e59b848ffc88a94f1c63e3a4baa82387ea4bfe
G-Henri <gleydson.silva.716@ufrn.edu.br>
2025-10-28 20:38:45 -0300

    Criação do arquivo validacao.h

---
+++ validacao.h
@@ -0,0 +1,22 @@
+#ifndef VALIDACAO_H
+#define VALIDACAO_H
+
+int ehLetra(char c);
+int ehDigito(char c);
+
+int validarNome(char *nome);
+int validarCPF(char *cpf);
+int validarEmail(char *email);
+int validarData(char *data);
+
+int validarPlaca(char *placa);
+int validarChassi(char *chassi);
+int validarRenavam(char *renavam);
+int validarCategoria(char *categoria);
+int validarModelo(char *modelo);
+int validarMarca(char *marca);
+int validarAno(char *ano);
+int validarCodigoInterno(char *codigo);
+int validarPreco(float preco);
+
+#endif
