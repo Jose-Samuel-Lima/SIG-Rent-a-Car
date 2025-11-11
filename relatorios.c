05985b01ea679f68d07266bedea1118bdd832157
G-Henri <gleydson.silva.716@ufrn.edu.br>
2025-11-10 21:58:09 -0300

    Função de verificar funcionários ativos no módulo relatório.c

--- relatorios.c
+++ relatorios.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include "funcionarios.h"
 #include "relatorios.h"
 
 int modulo_relatorio(void)
@@ -12,7 +14,7 @@ int modulo_relatorio(void)
         switch (esc)
         {
         case 1:
-            modulo_relatorio_funcionarios();
+            navegar_relatorio_funcionarios();
             break;
         case 2:
             modulo_relatorio_clientes();
@@ -28,6 +30,42 @@ int modulo_relatorio(void)
         }
 
     } while (esc != 0);
+    return -1;
+}
+
+int navegar_relatorio_funcionarios(void)
+{ 
+    int esc_funcionario;
+    do
+    {
+        esc_funcionario = modulo_relatorio_funcionarios();
+        switch(esc_funcionario)
+        {
+        case 1: 
+            funcionarios_ativos();
+            break;
+        case 0:
+            return -1;
+        }
+    } while (esc_funcionario != 0);
+    return -1;
+}
+
+int navegar_relatorio_clientes(void)
+{ 
+    int esc_cliente;
+    do
+    {
+        esc_cliente = modulo_relatorio_funcionarios();
+        switch(esc_cliente)
+        {
+        case 1: 
+            funcionarios_ativos();
+            break;
+        case 0:
+            return -1;
+        }
+    } while (esc_cliente != 0);
     return -1;
 }
 
@@ -66,9 +104,11 @@ int modulo_tela_relatorio(void)
     return op;
 }
 
-void modulo_relatorio_funcionarios(void)
+int modulo_relatorio_funcionarios(void)
 {
-    int 
+    int op;
+    int c;
+
     system("clear||cls");
     printf("\n");
     printf("#=======================================================================#\n");
@@ -81,15 +121,19 @@ void modulo_relatorio_funcionarios(void)
     printf("|                                                                       |\n");
     printf("|              < = = =  Relatório de Funcionários = = = >               |\n");
     printf("|                                                                       |\n");
-    printf("|                        # 1 # Relatório_Ativos                         |\n");
-    printf("|                        # 2 # Relatório_Geral                          |\n");
+    printf("|                       # 1 # Funcionários_Ativos                       |\n");
+    printf("|                       # 2 # Funcionários_Inativos                     |\n");
+    printf("|                       # 0 # Sair                                      |\n");
     printf("|                                                                       |\n");
     printf("#=======================================================================#\n");
     printf("\n");
-    getchar();
-    system("cls||clear");
-    printf("Pressione Enter para continuar...");
-    getchar();
+    printf("Escolha uma das opções acima: ");
+    scanf(" %d", &op);
+    while ((c = getchar()) != '\n' && c != EOF)
+        ;
+    printf("\n");
+    printf("Processando...\n");
+    return op;
 }
 
 void modulo_relatorio_clientes(void)
@@ -106,8 +150,9 @@ void modulo_relatorio_clientes(void)
     printf("|                                                                       |\n");
     printf("|                < = = =  Relatório de Clientes = = = >                 |\n");
     printf("|                                                                       |\n");
-    printf("|                         # 1 # Relatório_Ativos                        |\n");
-    printf("|                         # 2 # Relatório_Geral                         |\n");
+    printf("|                       # 1 # Clientes_Ativos                           |\n");
+    printf("|                       # 2 # Clientes_Inativos                         |\n");
+    printf("|                       # 0 # Sair                                      |\n");
     printf("|                                                                       |\n");
     printf("#=======================================================================#\n");
     printf("\n");
@@ -167,3 +212,56 @@ void modulo_relatorio_alugueis(void)
     getchar();
 
 }
+
+void funcionarios_ativos(void)
+{
+    FILE *arq_funcionario;
+    Funcionario* fun;
+    fun = (Funcionario*) malloc(sizeof(Funcionario));
+    arq_funcionario = fopen("funcionario.dat","rb");
+
+    system("clear||cls");
+    printf("\n");
+    printf("#=======================================================================#\n");
+    printf("|                                                                       |\n");
+    printf("|                 < = = =  Funcionários Ativos = = = >                  |\n");
+    printf("|                                                                       |\n");
+    printf("#=======================================================================#\n");
+    printf("\n");
+    
+    if (arq_funcionario == NULL) {
+        printf("XXX - Nenhum arquivo de funcionários encontrado!\n");
+        printf("[>] - Pressione Enter para continuar...");
+        getchar();
+        exit(1);
+    }
+
+    int contador = 0;
+
+    while (fread(fun, sizeof(Funcionario), 1, arq_funcionario)) {
+        if (fun->status == true) {
+            contador++;
+            printf("------------------------------------------------------------\n");
+            printf("Funcionário #%d\n", contador);
+            printf("Nome: %s\n", fun->nome_funcionario);
+            printf("CPF: %s\n", fun->cpf_funcionario);
+            printf("Data de Nascimento: %s\n", fun->dt_nascimento_fun);
+            printf("Email: %s\n", fun->email_funcionario);
+            printf("Cargo: %s\n", fun->cargo);
+        }
+    }
+
+    if (contador == 0) {
+        printf("XXX - Nenhum funcionário ativo encontrado!\n");
+    } else {
+        printf("------------------------------------------------------------\n");
+        printf("Total de funcionários ativos: %d\n", contador);
+    }
+
+    fclose(arq_funcionario);
+    free(fun);
+
+    printf("\n[>] - Pressione Enter para continuar...");
+    getchar();
+}
+
--- relatorios.h
+++ relatorios.h
@@ -1,9 +1,12 @@
-#ifndef RELATORIO_H
-#define RELATORIO_H
+#ifndef RELATORIOS_H
+#define RELATORIOS_H
 
 int modulo_relatorio(void);
 int modulo_tela_relatorio(void);
-void modulo_relatorio_funcionarios(void);
+int navegar_relatorio_funcionarios(void);
+int modulo_relatorio_funcionarios(void);
+void funcionarios_ativos(void);
+void funcionarios_inativos(void);
 void modulo_relatorio_clientes(void);
 void modulo_relatorio_veiculos(void);
 void modulo_relatorio_alugueis(void);
